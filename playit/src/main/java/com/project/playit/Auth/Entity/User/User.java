2749f86a7f534aac8266522c618dc9489923e586
Lipun <lipundani@gmail.com>
2024-06-30 17:07:02 +0530

    Revert "add getter for user authorities"
    This reverts commit 1776a710a0eb88a3c580fb6e0689ce93f3f098df.

--- playit/src/main/java/com/project/playit/Auth/Entity/User/User.java
+++ playit/src/main/java/com/project/playit/Auth/Entity/User/User.java
@@ -8,10 +8,8 @@ import lombok.Builder;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -51,7 +49,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+        return null;
     }
 
     @Override
