1776a710a0eb88a3c580fb6e0689ce93f3f098df
Lipun <lipundani@gmail.com>
2024-06-30 16:57:04 +0530

    add getter for user authorities

--- playit/src/main/java/com/project/playit/Auth/Entity/User/User.java
+++ playit/src/main/java/com/project/playit/Auth/Entity/User/User.java
@@ -8,8 +8,10 @@ import lombok.Builder;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -49,7 +51,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
