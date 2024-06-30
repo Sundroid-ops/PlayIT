1adcd5ddb17877a11e800d9face2aff4c83acf46
Lipun <lipundani@gmail.com>
2024-06-30 17:46:30 +0530

    add RestController for auth API
    - Implemented register feature for users to signup
    
    - Implemented authenticate feature for users to login back

---
+++ playit/src/main/java/com/project/playit/Auth/Controller/AuthenticateRequest.java
@@ -0,0 +1,7 @@
+package com.project.playit.Auth.Controller;
+
+public class AuthenticateRequest {
+    private String email;
+
+    private String password;
+}
---
+++ playit/src/main/java/com/project/playit/Auth/Controller/RegisterRequest.java
@@ -0,0 +1,13 @@
+package com.project.playit.Auth.Controller;
+
+public class RegisterRequest {
+    private String userName;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+}
