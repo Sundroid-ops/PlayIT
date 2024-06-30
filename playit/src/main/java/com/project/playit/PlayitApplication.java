949467d23ee668667d74b24cfaf949bf98455ba3
Lipun <lipundani@gmail.com>
2024-06-30 12:43:17 +0530

    add auth folder
    folder contains authorization and authentication of users

---
+++ playit/src/main/java/com/project/playit/PlayitApplication.java
@@ -0,0 +1,13 @@
+package com.project.playit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayitApplication.class, args);
+	}
+
+}
---
+++ playit/src/test/java/com/project/playit/PlayitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.playit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
