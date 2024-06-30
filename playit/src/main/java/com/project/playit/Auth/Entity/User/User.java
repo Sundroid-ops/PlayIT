2231072cf13a1a873123f94a1baebe91107d43e5
Lipun <lipundani@gmail.com>
2024-06-30 13:37:18 +0530

    add validations on user entity

--- playit/src/main/java/com/project/playit/Auth/Entity/User/User.java
+++ playit/src/main/java/com/project/playit/Auth/Entity/User/User.java
@@ -1,10 +1,15 @@
 package com.project.playit.Auth.Entity.User;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
@@ -19,14 +24,27 @@ public class User implements UserDetails {
     @Id
     private UUID ID;
 
+    @NotBlank(message = "UserName cannot be empty")
+    @Column(unique = true)
+    @Length(min = 3, max = 15, message = "UserName should have more than 2 and less than 16 character")
+    @NotNull(message = "UserName is required")
     private String userName;
 
+    @Length(min = 3, max = 15, message = "FirstName should have more than 2 and less than 16 character")
     private String firstName;
 
+    @Length(min = 3, max = 15, message = "LastName should have more than 2 and less than 16 character")
     private String lastName;
 
+    @NotNull(message = "Email is required")
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Email should be valid")
+    @Column(unique = true)
     private String email;
 
+    @NotNull(message = "Password is required")
+    @NotBlank(message = "Password cannot be empty")
+    @Length(min = 5, max = 15, message = "Password should have more than 4 and less than 16 character")
     private String password;
 
     @Override
