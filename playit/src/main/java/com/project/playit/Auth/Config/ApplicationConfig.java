422c4feb1f6e9d92f5afabdf214135cfcb9951d2
Lipun <lipundani@gmail.com>
2024-06-30 19:57:34 +0530

    add authenticationManager feature to authenticate user

--- playit/src/main/java/com/project/playit/Auth/Config/ApplicationConfig.java
+++ playit/src/main/java/com/project/playit/Auth/Config/ApplicationConfig.java
@@ -1,10 +1,18 @@
 package com.project.playit.Auth.Config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class ApplicationConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
