5765ad649628cb2eff4f5552a6a5200382a64165
Lipun <lipundani@gmail.com>
2024-06-30 18:33:02 +0530

    add user to userRepository

--- playit/src/main/java/com/project/playit/Auth/Service/AuthServiceImpl.java
+++ playit/src/main/java/com/project/playit/Auth/Service/AuthServiceImpl.java
@@ -1,14 +1,19 @@
 package com.project.playit.Auth.Service;
 
 import com.project.playit.Auth.Controller.RegisterRequest;
+import com.project.playit.Auth.Entity.User.Role;
 import com.project.playit.Auth.Entity.User.User;
 import com.project.playit.Auth.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class AuthServiceImpl implements AuthService{
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public void register(RegisterRequest request){
         User user = User.builder()
@@ -16,9 +21,10 @@ public class AuthServiceImpl implements AuthService{
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
                 .email(request.getEmail())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
                 .build();
 
-
+        userRepository.save(user);
     }
 }
