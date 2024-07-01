cfc31d7964542b9135348bc6d7a63358c53bcb18
Lipun <lipundani@gmail.com>
2024-07-01 17:36:00 +0530

    implemented userDetailsService bean to find user using emailId

--- playit/src/main/java/com/project/playit/Auth/Config/ApplicationConfig.java
+++ playit/src/main/java/com/project/playit/Auth/Config/ApplicationConfig.java
@@ -1,12 +1,25 @@
 package com.project.playit.Auth.Config;
 
+import com.project.playit.Auth.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class ApplicationConfig {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(String emailID){
+        return username -> userRepository.findByEmailID(emailID)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
