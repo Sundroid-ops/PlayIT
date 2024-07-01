05c664830d64e3c68f6f2e26c216e1f0987db730
Lipun <lipundani@gmail.com>
2024-07-01 18:30:11 +0530

    implemented authProvider to authenticate user

--- playit/src/main/java/com/project/playit/Auth/Config/ApplicationConfig.java
+++ playit/src/main/java/com/project/playit/Auth/Config/ApplicationConfig.java
@@ -23,6 +23,13 @@ public class ApplicationConfig {
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
     }
 
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
