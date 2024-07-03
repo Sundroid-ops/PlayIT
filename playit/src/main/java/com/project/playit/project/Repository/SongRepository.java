fb51e964cee71bb8f16f704023db78f77b378a56
Lipun <lipundani@gmail.com>
2024-07-03 12:28:10 +0530

    add songRepository to handle queries related to songs

---
+++ playit/src/main/java/com/project/playit/project/Repository/SongRepository.java
@@ -0,0 +1,11 @@
+package com.project.playit.project.Repository;
+
+import com.project.playit.project.Entity.Song;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SongRepository extends JpaRepository<Song, UUID> {
+}
