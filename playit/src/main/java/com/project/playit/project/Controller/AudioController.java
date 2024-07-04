2c7b6e116664a4e2d43b65b6589764065cb17b2e
Lipun <lipundani@gmail.com>
2024-07-04 17:37:57 +0530

    setup a audio post POST request to save user requested audio with name

--- playit/src/main/java/com/project/playit/project/Controller/AudioController.java
+++ playit/src/main/java/com/project/playit/project/Controller/AudioController.java
@@ -1,12 +1,14 @@
 package com.project.playit.project.Controller;
 
+import com.project.playit.project.DTO.AudioUploadRequest;
 import com.project.playit.project.Entity.Audio;
 import com.project.playit.project.Service.AudioService;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/song")
@@ -15,8 +17,8 @@ public class AudioController {
     @Autowired
     private AudioService songService;
 
-    @GetMapping("/")
-    public ResponseEntity<Audio> test(){
-        return ResponseEntity.ok(songService.uploadAudioFile());
+    @PostMapping("/upload")
+    public ResponseEntity<Audio> test(AudioUploadRequest request){
+        return ResponseEntity.ok(songService.uploadAudioFile(request));
     }
 }
