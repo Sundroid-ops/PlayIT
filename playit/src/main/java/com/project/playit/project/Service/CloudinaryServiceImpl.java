4f5d0d8737c3ece3cb0950a86d83245a9c4f1f9e
Lipun <lipundani@gmail.com>
2024-07-04 17:33:17 +0530

    fixed bug where audio stored url was of an image link, now getting the required video url

--- playit/src/main/java/com/project/playit/project/Service/CloudinaryServiceImpl.java
+++ playit/src/main/java/com/project/playit/project/Service/CloudinaryServiceImpl.java
@@ -1,12 +1,14 @@
 package com.project.playit.project.Service;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
+import com.cloudinary.utils.ObjectUtils;
 import jakarta.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,13 +20,12 @@ public class CloudinaryServiceImpl implements CloudinaryService{
     private Cloudinary cloudinary;
 
     @Override
-    public String uploadAudioFile(MultipartFile file, String folderName) {
+    public String uploadAudioFile(MultipartFile file, String audioName) {
         try {
-            Map<Object, Object> options = new HashMap<>();
-            options.put("songs", folderName);
-            Map uploadFile = cloudinary.uploader().upload(file.getBytes(), options);
-            String publicID = (String) uploadFile.get("public_id");
-            return cloudinary.url().secure(true).generate(publicID);
+            Map<String, Object> uploadFile = cloudinary.uploader().upload(file.getBytes(),
+                    ObjectUtils.asMap("resource_type", "auto",
+                                    "public_id", audioName));
+            return uploadFile.get("secure_url").toString();
 
         }catch (IOException e){
             e.printStackTrace();
