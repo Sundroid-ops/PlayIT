6d1f3ae3132f5792c79ca5c0d185f6021711f98b
Lipun <lipundani@gmail.com>
2024-07-05 18:23:27 +0530

    implemented the genre field in audioUpload DTO for user to choose the suitable genre for their audio

--- playit/src/main/java/com/project/playit/project/DTO/AudioUploadRequest.java
+++ playit/src/main/java/com/project/playit/project/DTO/AudioUploadRequest.java
@@ -1,5 +1,8 @@
 package com.project.playit.project.DTO;
 
+import com.project.playit.project.Entity.Genre;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -20,4 +23,9 @@ public class AudioUploadRequest {
     @NotNull(message = "name cannot be null")
     @NotBlank(message = "name cannot be empty")
     private MultipartFile file;
+
+    @NotNull(message = "genre cannot be null")
+    @NotBlank(message = "genre cannot be empty")
+    @Enumerated(EnumType.STRING)
+    private Genre genres;
 }
