38a425fe1879c7a69bcac3f399551dee78069cc7
Lipun <lipundani@gmail.com>
2024-07-05 17:46:35 +0530

    refactored songService to audioService and test method to uploadUserAudioFile

--- playit/src/main/java/com/project/playit/project/Controller/AudioController.java
+++ playit/src/main/java/com/project/playit/project/Controller/AudioController.java
@@ -2,6 +2,7 @@ package com.project.playit.project.Controller;
 
 import com.project.playit.project.DTO.AudioUploadRequest;
 import com.project.playit.project.Entity.Audio;
+import com.project.playit.project.Exception.AudioFileNotFoundException;
 import com.project.playit.project.Service.AudioService;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.validation.Valid;
@@ -10,15 +11,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/song")
 public class AudioController {
 
     @Autowired
-    private AudioService songService;
+    private AudioService audioService;
 
     @PostMapping("/upload")
-    public ResponseEntity<Audio> test(AudioUploadRequest request){
-        return ResponseEntity.ok(songService.uploadAudioFile(request));
+    public ResponseEntity<Audio> uploadUserAudioFile(AudioUploadRequest request){
+        return ResponseEntity.ok(audioService.uploadAudioFile(request));
     }
 }
