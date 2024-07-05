1021477b515329dfc00c0351a2e2bb3a69c63853
Lipun <lipundani@gmail.com>
2024-07-05 17:43:28 +0530

    add findAllByAudioNameContainingIgnoreCase in audioRepo to find list of audioData by their audio name

--- playit/src/main/java/com/project/playit/project/Repository/AudioRepository.java
+++ playit/src/main/java/com/project/playit/project/Repository/AudioRepository.java
@@ -4,9 +4,11 @@ import com.project.playit.project.Entity.Audio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AudioRepository extends JpaRepository<Audio, UUID> {
-
+    List<Audio> findAllByAudioNameContainingIgnoreCase(String audioName);
 }
