a7969f35a10618948f99e11f5bd78c184df0787c
Lipun <lipundani@gmail.com>
2024-07-05 17:45:07 +0530

    add getAudioListFromAudioName method in audioRepo to implement a service logic to get list of audio data when search by their audio name

--- playit/src/main/java/com/project/playit/project/Service/AudioService.java
+++ playit/src/main/java/com/project/playit/project/Service/AudioService.java
@@ -2,7 +2,12 @@ package com.project.playit.project.Service;
 
 import com.project.playit.project.DTO.AudioUploadRequest;
 import com.project.playit.project.Entity.Audio;
+import com.project.playit.project.Exception.AudioFileNotFoundException;
+
+import java.util.List;
 
 public interface AudioService {
     public Audio uploadAudioFile(AudioUploadRequest request);
+
+    public List<Audio> getAudioListFromAudioName(String audioName) throws AudioFileNotFoundException;
 }
--- playit/src/main/java/com/project/playit/project/Service/AudioServiceImpl.java
+++ playit/src/main/java/com/project/playit/project/Service/AudioServiceImpl.java
@@ -1,12 +1,16 @@
 package com.project.playit.project.Service;
 
+import com.fasterxml.jackson.annotation.OptBoolean;
 import com.project.playit.Auth.Service.CurrentUserService;
 import com.project.playit.project.DTO.AudioUploadRequest;
 import com.project.playit.project.Entity.Audio;
+import com.project.playit.project.Exception.AudioFileNotFoundException;
 import com.project.playit.project.Repository.AudioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -19,18 +23,28 @@ public class AudioServiceImpl implements AudioService {
     private CurrentUserService currentUserService;
 
     @Autowired
-    private AudioRepository songRepository;
+    private AudioRepository audioRepository;
 
     @Override
     public Audio uploadAudioFile(AudioUploadRequest request){
 
         Audio song = Audio.builder()
-                .songID(UUID.randomUUID())
-                .Name(request.getName())
+                .audioID(UUID.randomUUID())
+                .audioName(request.getName())
                 .file_url(cloudinaryService.uploadAudioFile(request.getFile(), request.getName()))
                 .user_upload(currentUserService.getCurrentUser())
                 .build();
 
-        return songRepository.save(song);
+        return audioRepository.save(song);
+    }
+
+    @Override
+    public List<Audio> getAudioListFromAudioName(String audioName) throws AudioFileNotFoundException {
+        List<Audio> audioList = audioRepository.findAllByAudioNameContainingIgnoreCase(audioName);
+
+        if(audioList.isEmpty())
+            throw new AudioFileNotFoundException("Audio Not Found");
+
+        return audioList;
     }
 }
