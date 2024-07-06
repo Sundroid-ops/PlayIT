996d185f429518078ec27ae6f7e7c27629598492
Lipun <lipundani@gmail.com>
2024-07-06 13:14:28 +0530

    uploadAudioFile throws IOException during file errors

--- playit/src/main/java/com/project/playit/project/Service/CloudinaryService.java
+++ playit/src/main/java/com/project/playit/project/Service/CloudinaryService.java
@@ -3,9 +3,12 @@ package com.project.playit.project.Service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Map;
 
 public interface CloudinaryService {
 
-    public Map<String, String> uploadAudioFile(MultipartFile file, String folderName);
+    public Map<String, String> uploadAudioFile(MultipartFile file, String folderName) throws IOException;
+
+    public void deleteAudioFile(String cloudinaryFilePublicID) throws IOException;
 }
