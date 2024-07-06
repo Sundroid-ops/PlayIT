da3060023c3c3d49bccd2126717514048125cb06
Lipun <lipundani@gmail.com>
2024-07-06 20:44:18 +0530

    implemented GET request for getPlayListByID method to search playlist by playlist ID

--- playit/src/main/java/com/project/playit/project/Controller/PlayListController.java
+++ playit/src/main/java/com/project/playit/project/Controller/PlayListController.java
@@ -4,10 +4,9 @@ import com.project.playit.project.Entity.PlayList;
 import com.project.playit.project.Service.PlayListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/playlist")
@@ -19,5 +18,10 @@ public class PlayListController {
     @PostMapping("/create")
     public ResponseEntity<PlayList> createPlayList(@RequestParam String playListName){
         return ResponseEntity.ok(playListService.createPlayList(playListName));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<PlayList> getPlayListByID(@PathVariable UUID playListID){
+        return ResponseEntity.ok(playListService.getPlayListByID(playListID));
     }
 }
