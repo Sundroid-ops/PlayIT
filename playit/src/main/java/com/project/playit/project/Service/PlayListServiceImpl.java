01b8f80ab3038153dc1122ae48746449dd2afcbb
Lipun <lipundani@gmail.com>
2024-07-06 20:43:50 +0530

    implemented getPlayListByID in playList service to search playlist by playlist ID

--- playit/src/main/java/com/project/playit/project/Service/PlayListServiceImpl.java
+++ playit/src/main/java/com/project/playit/project/Service/PlayListServiceImpl.java
@@ -2,6 +2,7 @@ package com.project.playit.project.Service;
 
 import com.project.playit.Auth.Service.CurrentUserService;
 import com.project.playit.project.Entity.PlayList;
+import com.project.playit.project.Exception.PlayListNotFoundException;
 import com.project.playit.project.Repository.PlayListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,11 @@ public class PlayListServiceImpl implements PlayListService{
                 .build();
 
         return playListRepository.save(playList);
+    }
 
+    @Override
+    public PlayList getPlayListByID(UUID playListID) {
+        return playListRepository.findById(playListID)
+                .orElseThrow(() -> new PlayListNotFoundException("PlayList Not Found for ID : " + playListID));
     }
 }
