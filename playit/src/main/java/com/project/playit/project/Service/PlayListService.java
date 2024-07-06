9b4cfea7ed9f48421f818fd0159323716a35cc68
Lipun <lipundani@gmail.com>
2024-07-06 20:31:28 +0530

    implemented createPlayList for adding audio to playlist in playlist service layer

---
+++ playit/src/main/java/com/project/playit/project/Service/PlayListService.java
@@ -0,0 +1,10 @@
+package com.project.playit.project.Service;
+
+import com.project.playit.project.Entity.PlayList;
+
+import java.util.UUID;
+
+public interface PlayListService {
+    public PlayList createPlayList(String playListName);
+
+}
---
+++ playit/src/main/java/com/project/playit/project/Service/PlayListServiceImpl.java
@@ -0,0 +1,33 @@
+package com.project.playit.project.Service;
+
+import com.project.playit.Auth.Service.CurrentUserService;
+import com.project.playit.project.Entity.PlayList;
+import com.project.playit.project.Repository.PlayListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Service
+public class PlayListServiceImpl implements PlayListService{
+
+    @Autowired
+    private PlayListRepository playListRepository;
+
+    @Autowired
+    private CurrentUserService currentUserService;
+
+    @Override
+    public PlayList createPlayList(String playListName) {
+        PlayList playList = PlayList.builder()
+                .playListID(UUID.randomUUID())
+                .playListName(playListName)
+                .creationDate(LocalDate.now())
+                .user_playList(currentUserService.getCurrentUser())
+                .build();
+
+        return playListRepository.save(playList);
+
+    }
+}
