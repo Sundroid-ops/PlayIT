f010be7c83ef75d9c693bc54920d2b189e899f21
Lipun <lipundani@gmail.com>
2024-07-07 11:20:49 +0530

    handling PlayListNotFoundException to handle playlist not found exceptions

--- playit/src/main/java/com/project/playit/project/Advice/RestResponseApplicationConfiguration.java
+++ playit/src/main/java/com/project/playit/project/Advice/RestResponseApplicationConfiguration.java
@@ -3,6 +3,7 @@ package com.project.playit.project.Advice;
 import com.project.playit.Auth.Entity.ErrorMessage;
 import com.project.playit.project.Exception.AccessDeniedException;
 import com.project.playit.project.Exception.AudioFileNotFoundException;
+import com.project.playit.project.Exception.PlayListNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class RestResponseApplicationConfiguration {
 
-    @ExceptionHandler(AudioFileNotFoundException.class)
+    @ExceptionHandler({AudioFileNotFoundException.class,
+                        PlayListNotFoundException.class})
     public ResponseEntity<ErrorMessage>  DataNotFoundException(Exception e){
         ErrorMessage error = new ErrorMessage(HttpStatus.NOT_FOUND, e.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
