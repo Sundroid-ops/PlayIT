be0af94a0cc6078147bb6bc2b3615c0afeae7a15
Lipun <lipundani@gmail.com>
2024-07-07 11:33:52 +0530

    implementing a addAudioFileInPlayList in playlist service interface to allow audio files to be added to playlist owned by the user

--- playit/src/main/java/com/project/playit/project/Service/PlayListService.java
+++ playit/src/main/java/com/project/playit/project/Service/PlayListService.java
@@ -1,12 +1,18 @@
 package com.project.playit.project.Service;
 
 import com.project.playit.project.Entity.PlayList;
+import com.project.playit.project.Exception.AccessDeniedException;
+import com.project.playit.project.Exception.AudioFileNotFoundException;
 import com.project.playit.project.Exception.PlayListNotFoundException;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PlayListService {
     public PlayList createPlayList(String playListName);
 
     public PlayList getPlayListByID(UUID playListID) throws PlayListNotFoundException;
+
+    public PlayList addAudioFileInPlayList(UUID playListID, List<UUID> audioListID)
+            throws PlayListNotFoundException, AudioFileNotFoundException, AccessDeniedException;
 }
