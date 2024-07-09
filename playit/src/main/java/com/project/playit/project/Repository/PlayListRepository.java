1470095b0af2baf9d2dcd454688461b9650cece3
Lipun <lipundani@gmail.com>
2024-07-09 12:15:36 +0530

    implemented findAllByPlayListNameContainingIgnoreCase method to find playlist by name

--- playit/src/main/java/com/project/playit/project/Repository/PlayListRepository.java
+++ playit/src/main/java/com/project/playit/project/Repository/PlayListRepository.java
@@ -1,11 +1,14 @@
 package com.project.playit.project.Repository;
 
 import com.project.playit.project.Entity.PlayList;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PlayListRepository extends JpaRepository<PlayList, UUID> {
+    List<PlayList> findAllByPlayListNameContainingIgnoreCase(String playListName, Pageable pageable);
 }
