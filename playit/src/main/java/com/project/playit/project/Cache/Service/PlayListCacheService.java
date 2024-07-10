3c627491d221d9e98882d7b1478cdb6bd4bd0d54
Lipun <lipundani@gmail.com>
2024-07-10 11:29:41 +0530

    implemented savePlaylist method which takes playlist parameter and save it in cache playlist, sets TTL of 10 mins and return object playlist

--- playit/src/main/java/com/project/playit/project/Cache/Service/PlayListCacheService.java
+++ playit/src/main/java/com/project/playit/project/Cache/Service/PlayListCacheService.java
@@ -1,4 +1,8 @@
 package com.project.playit.project.Cache.Service;
 
+import com.project.playit.project.Entity.PlayList;
+
 public interface PlayListCacheService {
+
+    public PlayList savePlayList(PlayList playList);
 }
--- playit/src/main/java/com/project/playit/project/Cache/Service/PlayListCacheServiceImpl.java
+++ playit/src/main/java/com/project/playit/project/Cache/Service/PlayListCacheServiceImpl.java
@@ -1,7 +1,24 @@
 package com.project.playit.project.Cache.Service;
 
+import com.project.playit.project.Entity.PlayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class PlayListCacheServiceImpl implements PlayListCacheService{
+
+    @Autowired
+    private RedisTemplate<String, PlayList> redisTemplate;
+
+    private final static String KEY = "PLAYLIST";
+
+    @Override
+    public PlayList savePlayList(PlayList playList) {
+        redisTemplate.opsForHash().put(KEY, playList.getPlayListID(), playList);
+        //redisTemplate.expire(KEY, 10, TimeUnit.MINUTES);
+        return playList;
+    }
 }
