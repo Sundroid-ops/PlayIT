28c5c9ca0588d7dbc4e7f5e363db3f08aada327c
Lipun <lipundani@gmail.com>
2024-07-12 19:10:50 +0530

    implemented jwt service impl test class to test jwt related logic

---
+++ playit/src/test/java/com/project/playit/Auth/Service/JwtServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.project.playit.Auth.Service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void extractUserName() {
+    }
+
+    @Test
+    void extractClaim() {
+    }
+
+    @Test
+    void isTokenValid() {
+    }
+
+    @Test
+    void isTokenExpiration() {
+    }
+
+    @Test
+    void generateToken() {
+    }
+}
