c159f2a83d76ba0f4974e9fcc0e981b7c5ec568a
Lipun <lipundani@gmail.com>
2024-07-14 12:05:29 +0530

    setup userRepo test to test manual setup repo queries

---
+++ playit/src/test/java/com/project/playit/Auth/Repository/UserRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.project.playit.Auth.Repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
+}
