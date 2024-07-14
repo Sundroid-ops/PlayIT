ce14b5eed68df9bb9efefddcae2676bf417ce090
Lipun <lipundani@gmail.com>
2024-07-14 12:29:35 +0530

    setup auth controller test class to test endpoints

---
+++ playit/src/test/java/com/project/playit/Auth/Controller/AuthControllerTest.java
@@ -0,0 +1,23 @@
+package com.project.playit.Auth.Controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(AuthController.class)
+class AuthControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void authenticate() {
+    }
+}
